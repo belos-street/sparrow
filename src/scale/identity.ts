6ddc0e744719ca5b0b36f5324f0972f827f23bce
aa1213889 <869135421@qq.com>
2023-07-24 23:47:48 +0800

    feat: add vitest

--- src/scale/identity.ts
+++ src/scale/identity.ts
@@ -1,5 +1,14 @@
-// Identity 是恒等映射，所以不需要指定定义域和值域
+/**
+ * Identity 恒等映射
+ *  将输入原封不动的返回
+ *  所以不需要指定定义域和值域
+ */
 
 export function createIdentity() {
   return <T>(x: T) => x
 }
+
+// const scale = createIdentity()
+// scale(1); // 1
+// scale({ a: 1 }); // { a: 1 }
+// scale('sparrow'); // 'sparrow'
