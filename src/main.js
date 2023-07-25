c7c65165490a57d39feba2adf4fd77aa87237099
aa1213889 <869135421@qq.com>
2023-07-25 22:57:24 +0800

    fix: edit createRenderer

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = '<div>233</div>'
--- src/scale/linear.ts
+++ src/scale/linear.ts
@@ -1,20 +1,28 @@
+import { interpolateNumber } from './utils/interpolate'
 import { normalize } from './utils/normalize'
 
 type Params = {
   domain: [number, number]
   range: [number, number]
-  interpolate: typeof interpolateNumber
+  interpolate?: typeof interpolateNumber
 }
 
 export function createLinear({ domain: [d0, d1], range: [r0, r1], interpolate = interpolateNumber }: Params) {
-  return (x: number) => {
+  const scale = (x: number) => {
     const t = normalize(x, d0, d1)
     // 默认是使用线性的数值插值器
     // 如果是颜色可以使用颜色插入器
     return interpolate(t, r0, r1)
   }
+
+  scale.ticks = (tickCount: number) => 5 + tickCount
+
+  return scale
 }
 
-export function interpolateNumber(t: number, start: number, stop: number) {
-  return start * (1 - t) + stop * t
-}
+const scale = createLinear({
+  domain: [0, 1],
+  range: [0, 1]
+})
+
+scale.ticks(10)
